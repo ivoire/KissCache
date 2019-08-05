f1cb914ce47346d52c9efee3864896261f5cb67d
Rémi Duraffort <remi.duraffort@linaro.org>
2019-08-05 15:29:32 +0200

    CI: set the right version string when building

--- .gitlab-ci/build/docker.sh
+++ .gitlab-ci/build/docker.sh
@@ -6,11 +6,12 @@ if [ "$1" = "setup" ]
 then
   set -x
   docker login -u gitlab-ci-token -p $CI_JOB_TOKEN $CI_REGISTRY
+  apk add git
 else
   set -x
 
   # Build the docker image
-  docker build -t $CI_REGISTRY_IMAGE:latest .
+  docker build -t $CI_REGISTRY_IMAGE:latest . --build-arg VERSION=$(git describe)
 
   # Push only for tags or master
   if [ "$CI_COMMIT_REF_SLUG" = "master" -o -n "$CI_COMMIT_TAG" ]
