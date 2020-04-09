df72a1f5e756e3077785882eadc15712b3cd95e6
Rémi Duraffort <remi.duraffort@linaro.org>
2020-04-09 15:00:02 +0200

    Remove debug leftover
    Also fix black formating.

--- kiss_cache/templatetags/kiss_cache.py
+++ kiss_cache/templatetags/kiss_cache.py
@@ -12,15 +12,20 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()
 
+
 @register.filter
 def sort_icon(order, value):
     attribute = order[1:] if order[0] == "-" else order
-    print(f"{attribute=}: {value=}")
     if attribute != value:
-        print("a link")
-        return mark_safe(f"""<a href="?order={value}"><i class="fas fa-sort-amount-up float-right"></i></a>""")
+        return mark_safe(
+            f"""<a href="?order={value}"><i class="fas fa-sort-amount-up float-right"></i></a>"""
+        )
 
     if order[0] == "-":
-        return mark_safe(f"""<a href="?order={value}"><i class="fas fa-sort-amount-down float-right"></i></a>""")
+        return mark_safe(
+            f"""<a href="?order={value}"><i class="fas fa-sort-amount-down float-right"></i></a>"""
+        )
     else:
-        return mark_safe(f"""<a href="?order=-{value}"><i class="fas fa-sort-amount-up-alt float-right"></i></a>""")
+        return mark_safe(
+            f"""<a href="?order=-{value}"><i class="fas fa-sort-amount-up-alt float-right"></i></a>"""
+        )
